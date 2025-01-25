0a8f1fae8c4f4a47203193ada69bdf7c4435d110
Paul-Dieter <pauldieter.brandt@yahoo.com>
2025-01-25 13:11:51 +0200

    Formatted Card Boxes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,31 @@
 <script>
+import FeedbackList from "./components/FeedbackList.svelte";
 
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'yeNaNa'
+		},
+		{
+			id: 2,
+			rating: 7,
+			text: 'yeNaNa'
+		},
+		{
+			id: 3,
+			rating: 5,
+			text: 'yeNaNa'
+		},
+		{
+			id: 4,
+			rating: 6,
+			text: 'yeNaNa'
+		},
+	]
 </script>
 
 <main>
-	<h1>Hello</h1>
-	
+	<FeedbackList {feedback}/>
 </main>
 
